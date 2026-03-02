d05cbd287c974b7784ee70638550be68d25a787d
Mihai Dragomir <mihaiadrian.dragomir@tide.co>
2026-03-02 13:03:02 +0200

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,9 +29,9 @@ public class Main {
             clientSocket = serverSocket.accept();
             DataOutputStream writer = new DataOutputStream(clientSocket.getOutputStream());
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String input = reader.readLine();
             KafkaResponse response = new KafkaResponse(7, 25);
-            writer.write(response.toByteArray());
+            byte[] responseBuf = response.toByteArray();
+            writer.write(responseBuf);
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
